def6b5536e673d396eac1a490bdfbd3153bbe69d
양희연 <deepbm27@gmail.com>
2021-04-19 10:35:10 +0900

    feat: Pick random menu

--- random_menu/index.js
+++ random_menu/index.js
@@ -11,3 +11,29 @@ function menuArr(...foods) {
 
 // 원하는 메뉴를 파라미터에 입력하세요
 const menu = menuArr('hamburger', 'sandwich', 'pizza');
+
+// 랜덤으로 메뉴 하나 선택
+function pickMenu(menu) {
+  const p = new Promise((resolve, reject) => {
+    if (menu.length === 0) {
+      reject(new Error('메뉴가 없습니다.'))
+    } else {
+      setTimeout(() => {
+        const randomIndex = Math.floor(Math.random() * menu.length)
+        resolve(menu[randomIndex])
+      }, 2000)
+    }
+  });
+  return p;
+}
+
+pickMenu(menu)
+  .then((menu) => {
+    console.log(`오늘의 점심메뉴는 ${menu.name}!!`);
+  })
+  .catch((error) => {
+    console.log(error.message);
+  })
+  .finally(() => {
+    console.log('매일 새로운 메뉴로 변경됩니다.');
+  });
