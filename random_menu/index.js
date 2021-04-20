6559db98ade91f658ddf21e1735b9a4e311e6722
양희연 <deepbm27@gmail.com>
2021-04-20 13:58:24 +0900

    Add async/await function

--- random_menu/index.js
+++ random_menu/index.js
@@ -10,7 +10,7 @@ function menuArr(...foods) {
 }
 
 // 원하는 메뉴를 파라미터에 입력하세요
-const menu = menuArr('hamburger', 'sandwich', 'pizza');
+const menus = menuArr('hamburger', 'sandwich', 'pizza');
 
 // 랜덤으로 메뉴 하나 선택
 function pickMenu(menu) {
@@ -27,7 +27,7 @@ function pickMenu(menu) {
   return p;
 }
 
-pickMenu(menu)
+pickMenu(menus)
   .then((menu) => {
     console.log(`오늘의 점심메뉴는 ${menu.name}!!`);
   })
@@ -37,3 +37,32 @@ pickMenu(menu)
   .finally(() => {
     console.log('매일 새로운 메뉴로 변경됩니다.');
   });
+
+
+// async/await 함수
+async function pickMenu(menu) {
+  const p = new Promise((resolve, reject) => {
+    if (menu.length === 0) {
+      reject(new Error('메뉴가 없습니다.'))
+    } else {
+      setTimeout(() => {
+        const randomIndex = Math.floor(Math.random() * menu.length)
+        resolve(menu[randomIndex])
+      }, 2000)
+    }
+  });
+  return p;
+}
+
+async function getMenu() {
+  try {
+    const menu = await pickMenu(menus);
+    console.log(`오늘의 점심메뉴는 ${menu.name}!!`)
+  } catch (error) {
+    console.log(error.message);
+  } finally {
+    console.log('매일 새로운 메뉴로 변경됩니다.');
+  }
+}
+
+getMenu();
