51d2bb24417ded82d03dca5bd9a3bddb03f8449d
양희연 <deepbm27@gmail.com>
2021-04-24 13:55:19 +0900

    Make form blank check event

--- modal/index.js
+++ modal/index.js
@@ -9,3 +9,14 @@ $('.btn-close').on('click', () => {
 $('.btn-nav-sub').on('click', () => {
   $('.nav-sub').slideToggle();
 });
+
+$('form').on('submit', e => {
+  if ($('.input-email').val() === '') {
+    e.preventDefault();
+    $('.alert-email').show();
+  }
+  if ($('.input-pw').val() === '') {
+    e.preventDefault();
+    $('.alert-pw').show();
+  }
+});
