b3f8b1236880cc938f6e43a68121489b70a8b3b6
양희연 <deepbm27@gmail.com>
2021-04-27 15:06:58 +0900

    Improve slide previous/next button event

--- modal/index.js
+++ modal/index.js
@@ -44,3 +44,38 @@ $('.btn-slide2').click(() => {
 $('.btn-slide3').click(() => {
   $('.slide-container').addClass('slide-200')
 });
+
+// let p = 0;
+// $('.btn-right').click(() => {
+//   if (p === 0) {
+//     p = 100;
+//     $('.slide-container').addClass('slide-100')
+//   } else if (p === 100) {
+//     p = 200;
+//     $('.slide-container').addClass('slide-200');
+//   }
+// });
+
+// $('.btn-left').click(() => {
+//   if (p === 100) {
+//     p = 0;
+//     $('.slide-container').removeClass('slide-100')
+//   } else if (p === 200) {
+//     $('.slide-container').removeClass('slide-200')
+//     $('.slide-container').addClass('slide-100');
+//     p = 100;
+//   }
+// });
+
+let n = 0;
+$('.btn-left').click(() => {
+  $('.slide-container').css('transform', `translateX(-${n-1}00vw)`);
+  n -= 1
+});
+
+$('.btn-right').click(() => {
+  if (n < 2) {
+    $('.slide-container').css('transform', `translateX(-${n+1}00vw)`)
+    n += 1
+  }
+});
