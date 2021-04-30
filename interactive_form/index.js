0c8e6cb83cefe07a072b9516ee406e81cb17dc6c
양희연 <deepbm27@gmail.com>
2021-04-30 17:53:44 +0900

    Improve select form by making HTML in JS

--- interactive_form/index.js
+++ interactive_form/index.js
@@ -1,7 +1,25 @@
+// $('#option1').on('change', () => {
+//   if ($('#option1').val() === '바지') {
+//     $('.select-size').show();
+//   } else {
+//     $('.select-size').hide();
+//   }
+// });
+
+
+// make HTML
 $('#option1').on('change', () => {
-  if ($('#option1').val() === '바지') {
-    $('.select-size').show();
-  } else {
-    $('.select-size').hide();
+  if ($('#option1').val() === '셔츠') {
+    $('#option2').html('');
+    let shirtsSize = `<option>95</option>
+    <option>100</option>
+    <option>105</option>`
+    $('#option2').append(shirtsSize);
+  } else if ($('#option1').val() === '바지') {
+    $('#option2').html('');
+    let pantsSize = `<option>28</option>
+    <option>30</option>
+    <option>32</option>`
+    $('#option2').append(pantsSize);
   }
 });
