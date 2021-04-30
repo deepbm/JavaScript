5f0344f7e06a0ffb090bbfdebef987fe027c8ee3
양희연 <deepbm27@gmail.com>
2021-04-30 19:03:01 +0900

    Improve select form by changing to forEach function

--- interactive_form/index.js
+++ interactive_form/index.js
@@ -8,18 +8,36 @@
 
 
 // make HTML
+// $('#option1').on('change', () => {
+//   if ($('#option1').val() === '셔츠') {
+//     $('#option2').html('');
+//     let shirtsSize = `<option>95</option>
+//     <option>100</option>
+//     <option>105</option>`
+//     $('#option2').append(shirtsSize);
+//   } else if ($('#option1').val() === '바지') {
+//     $('#option2').html('');
+//     let pantsSize = `<option>28</option>
+//     <option>30</option>
+//     <option>32</option>`
+//     $('#option2').append(pantsSize);
+//   }
+// });
+
+
+// forEach
 $('#option1').on('change', () => {
   if ($('#option1').val() === '셔츠') {
     $('#option2').html('');
-    let shirtsSize = `<option>95</option>
-    <option>100</option>
-    <option>105</option>`
-    $('#option2').append(shirtsSize);
+    let shirtsSize = [95, 100, 105];
+    shirtsSize.forEach(size => {
+      $('#option2').append(`<option>${size}</option>`);
+    });
   } else if ($('#option1').val() === '바지') {
     $('#option2').html('');
-    let pantsSize = `<option>28</option>
-    <option>30</option>
-    <option>32</option>`
-    $('#option2').append(pantsSize);
+    let pantsSize = [28, 30, 32, 34, 36];
+    pantsSize.forEach(size => {
+      $('#option2').append(`<option>${size}</option>`);
+    });
   }
 });
