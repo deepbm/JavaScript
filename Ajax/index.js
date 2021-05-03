0185344b8dfa838734d064bd75eac62ec4e7b93c
양희연 <deepbm27@gmail.com>
2021-05-03 23:00:41 +0900

    Bind data using Ajax

--- Ajax/index.js
+++ Ajax/index.js
@@ -1,8 +1,19 @@
-$('.btn').click(() => {
+// $('.btn').click(() => {
+//   $.ajax({
+//     url: 'https://codingapple1.github.io/hello.txt',
+//     type: 'GET'
+//   }).done(data => {
+//     $('#greeting').html(data);
+//   });
+// });
+
+$('.btn-dark').click(() => {
   $.ajax({
-    url: 'https://codingapple1.github.io/hello.txt',
+    url: 'https://codingapple1.github.io/data.json',
     type: 'GET'
   }).done(data => {
-    $('#greeting').html(data);
+    $('.card-img-top').attr('src', data.img);
+    $('.card-title').html(data.model);
+    $('.card-text').html(data.price);
   });
 });
