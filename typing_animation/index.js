95eb1854fbaaea9b66d3d8978ef16faf82c5239f
양희연 <deepbm27@gmail.com>
2021-05-04 20:53:21 +0900

    Make typing animation

---
+++ typing_animation/index.js
@@ -0,0 +1,13 @@
+const tag = document.querySelector('h1');
+const letter = document.querySelector('h1').innerHTML;
+
+$('.btn').click(() => {
+  tag.innerHTML = '';
+
+  for (let i = 0; i < letter.length; i++) {
+    setTimeout(() => {
+      tag.innerHTML = tag.innerHTML + letter[i];
+    }, 500 * i);
+  }
+  
+});
