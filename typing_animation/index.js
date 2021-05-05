08c61bb8cffd5c80b29481cdb5e1ca1a44d83eae
양희연 <deepbm27@gmail.com>
2021-05-05 11:28:13 +0900

    Improve typing animation to function

--- typing_animation/index.js
+++ typing_animation/index.js
@@ -2,6 +2,19 @@ const tag = document.querySelector('h1');
 const letter = document.querySelector('h1').innerHTML;
 
 $('.btn').click(() => {
+  // tag.innerHTML = '';
+
+  // for (let i = 0; i < letter.length; i++) {
+  //   setTimeout(() => {
+  //     tag.innerHTML = tag.innerHTML + letter[i];
+  //   }, 500 * i);
+  // }
+
+  typingAnimation(tag, letter);
+  
+});
+
+function typingAnimation(tag, letter) {
   tag.innerHTML = '';
 
   for (let i = 0; i < letter.length; i++) {
@@ -9,5 +22,4 @@ $('.btn').click(() => {
       tag.innerHTML = tag.innerHTML + letter[i];
     }, 500 * i);
   }
-  
-});
+}
